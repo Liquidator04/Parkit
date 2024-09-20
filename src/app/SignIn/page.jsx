961d74229f18805d5944ca0193b4f52b96256d23
wanderer04 <Adarshmohapatra10@gmail.com>
2024-09-20 18:20:36 +0530

    sigin page done

--- src/app/SignIn/page.jsx
+++ src/app/SignIn/page.jsx
@@ -1,7 +1,13 @@
 import SignIn from "@/components/sign-in";
+import { BackgroundBoxesDemo } from "@/components/backgroundsignin";
 function page() {
   return (
     <div>
+      
+      <BackgroundBoxesDemo/>
+      <br/>
+      <br/>
+      <br/>
       <SignIn />
     </div>
   );
--- src/components/sign-in.jsx
+++ src/components/sign-in.jsx
@@ -15,19 +15,19 @@ import {
 
 export default function SignIn() {
   return (
-    <div className="flex h-screen w-full items-center justify-center">
+    <div className="flex  w-full items-center justify-center h-40 ">
       <Card className="w-[300px]">
         <CardHeader>
           <CardTitle className="flex items-center justify-center mb-4">Sign In</CardTitle>
           <CardDescription className="flex items-center justify-center">Log in to your Parkit account</CardDescription>
         </CardHeader>
         <CardContent>
-        <form className="flex items-center justify-center"
+        <form className="flex items-center justify-center "
         action={async () => {
           "use server";
           await signIn("google");
         }}>
-          <Button className="flex w-75% gap-3 items-center justify-center" type="submit"><FaGoogle /><p>Sign in with Google</p></Button>
+          <Button className="flex w-75% gap-3 items-center justify-center " type="submit"><FaGoogle /><p>Sign in with Google</p></Button>
         </form>
         </CardContent>
       </Card>
